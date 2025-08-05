78447696a3bec467c25f1f2880b8c665b26a62a5
Godstin Paul <godstinpaul620@gmail.com>
2025-08-05 20:54:39 +0530

    Added heart animations and pulsing countdown

--- script.js
+++ script.js
@@ -23,4 +23,18 @@ function startCountdown() {
   }
   
   startCountdown();
+  function createFloatingHearts() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 5 + 5 + "s";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 10000);
+}
+
+setInterval(createFloatingHearts, 300);
+
   
